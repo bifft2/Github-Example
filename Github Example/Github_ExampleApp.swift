37ff1bf65b56ff1eb19a3945a94535b4a87df113
Brianna <bifft95@gmail.com>
2023-07-14 08:16:36 -0600

    Initial Commit

---
+++ Github Example/Github_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExampleApp.swift
+//  Github Example
+//
+//  Created by scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
